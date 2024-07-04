306526f34822abee1db80b47ce691dd8a0a9b1c8
dinei84 <devdinei@gmail.com>
2024-07-04 15:02:16 -0300

    Ainda não funciona o firebase

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,59 @@
-function logout(){
-    firebase.auth().signOut().then(()=>{
-        window.location.href = '../../index.html'
-    }).catch(()=>{
-        alert ('Erro ao fazer o Logout')
-    })
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = '../../index.html';
+    }).catch(() => {
+        alert('Erro ao fazer o Logout');
+    });
+}
+
+findTransactions();
+
+function findTransactions() {
+    firebase.firestore()
+        .collection('transaction')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => doc.data());
+            addTransactionsToScreen(transactions);
+        })
+        .catch(error => {
+            console.error('Error getting documents: ', error);
+        });
+}
+
+function addTransactionsToScreen(transactions) {
+    const orderedList = document.getElementById('transactions');
+
+    transactions.forEach(transaction => {
+        const li = document.createElement('li');
+        li.classList.add(transaction.type);
+
+        const date = document.createElement('p');
+        date.innerHTML = formatDate(transaction.date);
+        li.appendChild(date);
+
+        const money = document.createElement('p');
+        money.innerHTML = formatMoney(transaction.money);
+        li.appendChild(money);
+
+        const type = document.createElement('p');
+        type.innerHTML = transaction.transactionType;
+        li.appendChild(type);
+
+        if (transaction.description) {
+            const description = document.createElement('p');
+            description.innerHTML = transaction.description;
+            li.appendChild(description);
+        }
+
+        orderedList.appendChild(li);
+    });
+}
+
+function formatDate(date) {
+    return new Date(date).toLocaleDateString('pt-BR');
+}
+
+function formatMoney(money) {
+    return `${money.currency} ${money.value.toFixed(2)}`;
 }
